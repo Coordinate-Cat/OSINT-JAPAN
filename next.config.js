049aed3cb33cd72d1d336c5db068013e9de6446e
Coordinate-Cat <tetrapasta02@gmail.com>
2023-10-29 11:22:22 +0900

    feat: meta

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  i18n: {
+    locales: ["ja"],
+    defaultLocale: "ja",
+  },
+  compiler: {
+    styledComponents: true,
+  },
   reactStrictMode: true,
   pageExtensions: ["page.tsx", "page.ts"],
 };
--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,10 +1,20 @@
 import "@/styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>トップページ</title>
+        <meta
+          name="description"
+          property="og:title"
+          content="Resource site for osint in Japan."
+          key="title"
+        />
+      </Head>
       <Component {...pageProps} />
       <Analytics />
     </>
